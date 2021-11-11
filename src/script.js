8076c7de26e7d5b9f6d4e66ceb4ca284045cbb6e
Andrey Willian <andreywillianlemos@hotmail.com>
2021-11-11 15:03:36 -0300

    Adicionando todos documentos necessários para a execução da landingpage

---
+++ src/script.js
@@ -0,0 +1,18 @@
+function linkedin() {
+    window.open('https://www.linkedin.com/in/andrey-willian-847659169/');
+}
+function github() {
+    window.open('https://github.com/Andreywrl')
+}
+function instagram() {
+    window.open('https://instagram.com/andrey_wrl/')
+}
+function curriculum() {
+    window.open('https://drive.google.com/file/d/1A9khqcTEgpSoT18QC99q8AcXrnHAUjnt/view?usp=sharing')
+}
+function whatsapp() {
+    window.open('https://api.whatsapp.com/send?phone=5551994498561&text=Ol%C3%A1%2C%20Andrey!%20Tudo%20certo%20contigo%3F')
+}
+function curso() {
+    window.open('https://www.cursoemvideo.com/)')
+}
